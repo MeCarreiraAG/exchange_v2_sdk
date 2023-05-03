d720515b7022a42e8b80fc07580c8b4e3b658765
DevMania <devmania1223@gmail.com>
2023-05-03 15:23:47 +0800

    modifed init_code_hash

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ import JSBI from 'jsbi'
 
 export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
 
-export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
+export const INIT_CODE_HASH = '0xfc5b8d328e3f33f2bd2088f8417eb124156415abb58037f2adceab38108d5c54'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
